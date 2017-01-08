dbd27309c2c0f13d3698f51c5c79e94485f24675
m4s0 <neoomy@gmail.com>
2017-01-08 22:54:19 +0100

    add gulpfile.js and tasks

---
+++ frontend/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var size = require('gulp-size');
+var sourcemaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
+var path = require('path');
+
+gulp.task('css', function () {
+    return gulp.src(['./custom-bootstrap.less', 'vendor/components-font-awesome/less/font-awesome.less'])
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(cssmin())
+        .pipe(concat('main.min.css'))
+        .pipe(size())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src(['vendor/jquery/dist/jquery.js', 'vendor/bootstrap/dist/js/bootstrap.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(size())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('vendor/components-font-awesome/fonts/**.*')
+        .pipe(size())
+        .pipe(gulp.dest('./public/fonts'));
+});
